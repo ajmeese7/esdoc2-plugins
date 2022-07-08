ff2eaa5739faab44cc82c1959787c511f316822c
Aaron Meese <ajmeese7@gmail.com>
2022-07-07 20:54:33 -0400

    Merged #64 from OP
    Props to @darko-dev for this contribution.

--- packages/esdoc2-react-plugin/src/Plugin.js
+++ packages/esdoc2-react-plugin/src/Plugin.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const cheerio = require('cheerio');
-const ParamParser = require('esdoc2/out/src/Parser/ParamParser').default;
-const DocBuilder = require('esdoc2-publish-html-plugin/out/src/Builder/DocBuilder').default;
+const path = require("path");
+const cheerio = require("cheerio");
+const ParamParser = require("esdoc2/out/src/Parser/ParamParser").default;
+const DocBuilder = require("esdoc2-publish-html-plugin/out/src/Builder/DocBuilder").default;
 
 class Plugin {
   constructor() {
@@ -14,10 +14,10 @@ class Plugin {
 
     const reactPropsDocs = [];
     for (const doc of ev.data.docs) {
-      if (doc.kind !== 'class') continue;
+      if (doc.kind !== "class") continue;
       if (!doc.unknown) continue;
 
-      const reactProps = doc.unknown.filter(v => v.tagName === '@reactProps');
+      const reactProps = doc.unknown.filter(v => v.tagName === "@reactProps");
       if (!reactProps.length) continue;
 
       reactPropsDocs.push({
@@ -35,28 +35,28 @@ class Plugin {
     const fileName = ev.data.fileName;
 
     // only html
-    if (path.extname(fileName) !== '.html') return;
+    if (path.extname(fileName) !== ".html") return;
 
     // find target doc
     const doc = this._reactPropsDocs.find(doc => {
       const regexp = new RegExp(`${doc.fileName}$`);
-      if (fileName.match(regexp)) return true;
+      if (fileName.replace(/\\/g, "/").match(regexp)) return true;
     });
     if (!doc) return;
 
     // create esdoc2 properties from react props
     const properties = doc.reactProps.map(reactProp => {
-      const {typeText, paramName, paramDesc} = ParamParser.parseParamValue(reactProp.tagValue);
+      const { typeText, paramName, paramDesc } = ParamParser.parseParamValue(reactProp.tagValue);
       return ParamParser.parseParam(typeText, paramName, paramDesc);
     });
 
     // hack: create html
     const docBuilder = DocBuilder.createDefaultBuilder();
-    const html = docBuilder._buildProperties(properties, 'React Props:').html;
+    const html = docBuilder._buildProperties(properties, "React Props:").html;
 
     // append html
     const $ = cheerio.load(content);
-    $('.self-detail.detail').append(html);
+    $(".self-detail.detail").append(html);
 
     ev.data.content = $.html();
   }
